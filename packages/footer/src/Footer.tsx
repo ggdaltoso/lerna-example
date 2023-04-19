bc0c617e36f8565d4973fb600c2a7b7e71e97c42
ggdaltoso <ggdaltoso@gmail.com>
2023-04-19 11:40:44 -0300

    feat: update everything to work with an nextjs app

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+export function Footer({ styles, inter }) {
+  return (
+    <footer className={styles.grid}>
+      <a
+        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        className={styles.card}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className={inter.className}>
+          This <span>-&gt;</span>
+        </h2>
+        <p className={inter.className}>Just to</p>
+      </a>
 
-export function Footer() {
-    return (
-        <footer>
-            Footer
-        </footer>
-    );
+      <a
+        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        className={styles.card}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className={inter.className}>
+          Is <span>-&gt;</span>
+        </h2>
+        <p className={inter.className}>demonstrate</p>
+      </a>
+
+      <a
+        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        className={styles.card}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className={inter.className}>
+          The footer <span>-&gt;</span>
+        </h2>
+        <p className={inter.className}>a simple yarn workspace</p>
+      </a>
+
+      <a
+        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        className={styles.card}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className={inter.className}>
+          Package <span>-&gt;</span>
+        </h2>
+        <p className={inter.className}>project</p>
+      </a>
+    </footer>
+  );
 }
---
+++ packages/header/src/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      This is the header from <code>packages/header</code>
+    </header>
+  );
+}
